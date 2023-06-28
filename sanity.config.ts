d7831d325760d3ed20e83f62cac3ffaad6384436
unknown <jinhan.loh.2021@scis.smu.edu.sg>
2023-06-28 17:48:39 +0800

    Embed sanity in NextJS 13 app. Use NPM instead of Yarn. yarn doesn't work, styled components dependency issue. Config for sanity. Skeleton site and studio.

---
+++ sanity.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'sanity';
+import { deskTool } from 'sanity/desk';
+import { schemaTypes } from './sanity/schemas';
+import { codeInput } from '@sanity/code-input';
+
+const config = defineConfig({
+  title: 'Dothack CMS',
+
+  projectId: 'fdfze30c',
+  dataset: 'production',
+
+  apiVersion: '2023-06-28',
+  basePath: '/admin',
+
+  plugins: [deskTool(), codeInput()],
+
+  schema: { types: schemaTypes },
+});
+
+export default config;
---
+++ sanity/client.config.ts
@@ -0,0 +1,8 @@
+const clientConfig = {
+  projectId: 'fdfze30c',
+  dataset: 'production',
+  apiVersion: '2023-06-28',
+  useCdn: true,
+};
+
+export default clientConfig;
